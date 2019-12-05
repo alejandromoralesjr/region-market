7d11cf2ce432db798dea6e2c282df55381412f73
Alex Morales <moralesalex28@gmail.com>
2019-12-05 10:31:35 -0600

    removed log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const findRegionByState = require("./findRegionByState");
 const findStatesInRegion = require("./findStatesInRegion");
 const getAllRegions = require("./getAllRegions");
-console.log(getAllRegions());
 module.exports = {
   getAllRegions,
   findRegionByState,
